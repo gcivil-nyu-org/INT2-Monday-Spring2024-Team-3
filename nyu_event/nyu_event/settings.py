e8b1704c1f511b29a1e551abc915a66427e7d8cd
杨天凡 <ty2492@nyu.edu>
2024-02-23 15:43:43 -0500

    updates for eb

--- nyu_event/nyu_event/settings.py
+++ nyu_event/nyu_event/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--1vz#!np5i3v)p_(cclo9t8a40^ufcl#!tdkfq$p@6-=)ck@gf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nyu-event2-dev.us-east-1.elasticbeanstalk.com',]
+ALLOWED_HOSTS = ['nyu-event2-dev.us-east-1.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
@@ -77,16 +77,28 @@ WSGI_APPLICATION = 'nyu_event.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'nyu_event',
-            'USER': 'postgres',
-            'PASSWORD': 'Ytf@0930',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
- }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'nyu_event',
+                'USER': 'postgres',
+                'PASSWORD': 'Ytf@0930',
+                'HOST': 'localhost',
+                'PORT': '5432',
+        }
+    }
     
 
 
