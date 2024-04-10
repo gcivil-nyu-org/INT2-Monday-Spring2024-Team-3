6d8318198cf475c98fe934b2a8423a8e574c79f1
Yiyang Zhou <vial0520@gmail.com>
2024-04-10 03:21:56 -0400

    black

--- backend/huggingface/censorbot.py
+++ backend/huggingface/censorbot.py
@@ -1,7 +1,8 @@
 from sagemaker.huggingface import HuggingFacePredictor
 
-endpoint_name = 'huggingface-pytorch-inference-2024-04-10-04-28-53-172'
+endpoint_name = "huggingface-pytorch-inference-2024-04-10-04-28-53-172"
 predictor = HuggingFacePredictor(endpoint_name=endpoint_name)
+
 
 def detect_hate_speech(text: str):
     response = predictor.predict({"inputs": text})
