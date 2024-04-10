f50810f46c05f0f8d87227c31e0e2270c52bf2ed
Yiyang Zhou <vial0520@gmail.com>
2024-04-10 01:01:16 -0400

    fix ml

--- backend/huggingface/censorbot.py
+++ backend/huggingface/censorbot.py
@@ -1,9 +1,8 @@
-from transformers import pipeline
+from sagemaker.huggingface import HuggingFacePredictor
 
+endpoint_name = 'huggingface-pytorch-inference-2024-04-10-04-28-53-172'
+predictor = HuggingFacePredictor(endpoint_name=endpoint_name)
 
 def detect_hate_speech(text: str):
-    classifier = pipeline(
-        "text-classification", model="facebook/roberta-hate-speech-dynabench-r4-target"
-    )
-    res = classifier(text)
-    return res
+    response = predictor.predict({"inputs": text})
+    return response
